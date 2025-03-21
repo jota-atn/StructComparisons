7ce1f403101068fb049855987d21a6a64ecd13cc
123210584 JOAO ANTONIO DE MELO QUEIROZ <joao.antonio.melo.queiroz@lcc3-003.lcc.ufcg.edu.br>
2025-03-21 10:38:02 -0300

    consertando main.py

--- Python/Main.py
+++ Python/Main.py
@@ -6,25 +6,30 @@ from LinkedList import LinkedList
 import time
 import sys
 
-#arraylist = ArrayList(5)
-#bst = BST()
-#hashmap = HashMap()
-#linkedlist = LinkedList()
-
 arraylist = ArrayList(20)
 
-input_1000 = "../scripts/inputs/dataset_1000.txt"
-#total_time = 0
+input_1000 = "dataset_1000.txt"
+
+print("RODOU")
 
 with open(input_1000, "r") as archive:
-    #for i in range(500):
-    total_time = 0
-    inicio = time.time()
-    for line in archive:
-        data = int(line.strip())
-        arraylist.add(data)
+    linhas = archive.readlines()
+   
+print("LEU")
+    
+total_time = 0
+i = 0
+for linha in linhas:
+    if i == 0:
+        print("ENTROU")
+    inicio = time.time()    
+    arraylist.add(linha)
     fim = time.time()
-    total_time += fim - inicio
-    #media = total_time / 500
-    print(f"ArrayList com inserção de 1000 elementos: {total_time} segundos de execução")
+    total_time += (fim - inicio)
+    print(i)
+    i+=1
+    
+          
+total_time = (total_time) / len(linhas)
 
+print(f"tempo total para adicao no arralist {total_time}")
