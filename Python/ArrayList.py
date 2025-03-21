02e642ab7317b36ceda9426fbc298afb06521e99
Gabriel <gabrielfigueiredobm@hotmail.com>
2025-03-21 10:39:27 -0300

    .

--- Python/ArrayList.py
+++ Python/ArrayList.py
@@ -1,7 +1,7 @@
 class ArrayList:
 
     def __init__(self, size):
-        self.array = [None for i in range(size)]
+        self.array = []
         self.last = None
         self._size = 0
 
@@ -12,20 +12,12 @@ class ArrayList:
         return len(self) == 0
 
     def add(self, valor):
-        self.ensure_capacity(len(self.array) + 1)
-        if self.is_empty():
-            self.last = 0
-
-        self.array[self.last] = valor
-        self.last += 1
+        self.array.append(valor)
         self._size += 1
 
     def remove(self):
-        removed = self.array[self.last]
-        if len(self) == 1:
-            self.last = None
-        else:
-            self.last -= 1
+        removed = self.array[-1]
+        self.array.pop()
         self._size -= 1
         return removed
 
@@ -34,13 +26,3 @@ class ArrayList:
             return "Index out of bounds"
 
         return self.array[index]
-
-    def resize(self):
-        aux = [None for i in range(len(self.array) * 2)]
-        for i in range(len(self.array)):
-            aux[i] = self.array[i]
-        self.array = aux
-
-    def ensure_capacity(self, capacidade_pretendida):
-        if capacidade_pretendida > len(self.array):
-            self.resize()
