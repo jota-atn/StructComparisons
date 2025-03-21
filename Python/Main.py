ccb93c67bb34d5c7f78d15ef2f9aaaa29a6dab79
123210584 JOAO ANTONIO DE MELO QUEIROZ <joao.antonio.melo.queiroz@lcc3-003.lcc.ufcg.edu.br>
2025-03-21 10:45:41 -0300

    tests adicoes

--- Python/Main.py
+++ Python/Main.py
@@ -8,28 +8,21 @@ import sys
 
 arraylist = ArrayList(20)
 
-input_1000 = "dataset_1000.txt"
+sizes = [1000, 10000, 100000, 1000000, 10000000]
 
-print("RODOU")
-
-with open(input_1000, "r") as archive:
-    linhas = archive.readlines()
-   
-print("LEU")
+for size in sizes:
+    with open(f"../scripts/inputs/dataset_{size}.txt", "r") as archive:
+        linhas = archive.readlines()
     
-total_time = 0
-i = 0
-for linha in linhas:
-    if i == 0:
-        print("ENTROU")
-    inicio = time.time()    
-    arraylist.add(linha)
-    fim = time.time()
-    total_time += (fim - inicio)
-    print(i)
-    i+=1
-    
-          
-total_time = (total_time) / len(linhas)
+    total_time = 0
 
-print(f"tempo total para adicao no arralist {total_time}")
+    for linha in linhas:
+        inicio = time.time()    
+        arraylist.add(linha)
+        fim = time.time()
+        total_time += (fim - inicio)
+        
+            
+    total_time = (total_time) / len(linhas)
+
+    print(f"tempo total para adicao no arraylist de tamanho {size}: {total_time}")
