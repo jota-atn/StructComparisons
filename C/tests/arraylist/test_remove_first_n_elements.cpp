c291ac0988d35e66891305377b52f905cda0647f
Jota Antonio <jamqueiroz2005@gmail.com>
2025-04-05 20:45:12 -0300

    remove tests

---
+++ C/tests/arraylist/test_remove_first_n_elements.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <chrono>
+#include <string>
+#include "../../include/cpp/array_list.hpp"
+#include "../../include/cpp/output.hpp"
+
+using namespace std;
+using namespace chrono;
+
+auto test_remove_first_n_elements(ArrayList& array_list, int n) {
+
+    auto inicio = high_resolution_clock::now();
+    
+    for (int i = 0; i < n; i++) {
+        array_list.remove(0);
+    }
+    
+    auto fim = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(fim - inicio);
+    
+    return duration.count();
+}
+
+void test_dataset_remove_first_n_elements(ArrayList& array_list, int num_execucoes) {
+    
+    vector <int> values = {1000, 10000, 100000, 250000, 500000, 600000, 750000, 1000000, 1700000, 2500000, 3700000, 5000000, 6000000, 7500000, 9000000, 10000000};
+    
+    string saida = "C:/Users/jamqu/Projects/StructComparisons/C/out/remove_first_n_elements.txt";
+    
+    limpar_arquivo(saida);
+    
+    for (int valor : values) {
+        array_list.fill_array(to_string(valor));
+
+        long long tempo_total = 0;
+        
+        int num_elementos = valor * 0.001;
+        for (int i = 0; i < num_execucoes; ++i) {
+            tempo_total += test_remove_first_n_elements(array_list, num_elementos);
+        }
+
+        double tempo_medio = static_cast<double> (tempo_total) / num_execucoes;
+
+        gerar_saida(tempo_medio, to_string(valor), saida);
+
+        cout << "Tempo medio de remocao no fim de " << num_elementos
+            << " para um Data Set de tamanho " << to_string(valor)
+            << " apos " << num_execucoes << " execucoes: " << tempo_medio << " microssegundos" << endl;
+    }
+}
---
+++ C/tests/arraylist/test_remove_last_one_element.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <chrono>
+#include <string>
+#include "../../include/cpp/array_list.hpp"
+#include "../../include/cpp/output.hpp"
+
+using namespace std;
+using namespace chrono;
+
+auto test_remove_last_one_element(ArrayList& array_list) {
+
+    auto inicio = high_resolution_clock::now();
+    
+    array_list.remove(array_list.size() - 1);
+    
+    auto fim = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(fim - inicio);
+    
+    return duration.count();
+}
+
+void test_dataset_remove_last_one_element(ArrayList& array_list, int num_execucoes) {
+    
+    vector <int> values = {1000, 10000, 100000, 250000, 500000, 600000, 750000, 1000000, 1700000, 2500000, 3700000, 5000000, 6000000, 7500000, 9000000, 10000000};
+    
+    string saida = "C:/Users/jamqu/Projects/StructComparisons/C/out/remove_last_one_element.txt";
+    
+    limpar_arquivo(saida);
+    
+    for (int valor : values) {
+        array_list.fill_array(to_string(valor));
+
+        long long tempo_total = 0;
+        
+        for (int i = 0; i < num_execucoes; ++i) {
+            tempo_total += test_remove_last_one_element(array_list);
+        }
+
+        double tempo_medio = static_cast<double> (tempo_total) / num_execucoes;
+
+        gerar_saida(tempo_medio, to_string(valor), saida);
+
+        cout << "Tempo medio de remocao no fim de 1 elemento para um Data Set de tamanho " << to_string(valor)
+            << " apos " << num_execucoes << " execucoes: " << tempo_medio << " microssegundos" << endl;
+    }
+}
