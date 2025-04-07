42b2c44eb7d277218862c73633bd266269ce2d2b
Jota Antonio <jamqueiroz2005@gmail.com>
2025-04-06 21:53:59 -0300

    array list tests finalizados

--- C/tests/arraylist/test_get_element_inicio.cpp
+++ C/tests/arraylist/test_get_element_inicio.cpp
@@ -23,7 +23,7 @@ auto test_get_inicio(ArrayList& array_list, int n) {
 }
 
 void test_dataset_get_inicio(ArrayList& array_list) {
-    vector<int> values = {1000, 10000, 100000, 1000000, 10000000};
+    vector<int> values = {10000, 100000, 250000, 500000, 600000, 750000, 1000000, 1700000, 2500000, 3700000, 5000000, 6000000, 7500000, 9000000, 10000000};
     
     string saida = "../out/arraylist/get_inicio.txt";
     
