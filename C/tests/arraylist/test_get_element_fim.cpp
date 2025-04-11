a300d702675779e599580d6c04fea9af13316c47
PedroRamos1208 <pneto1999@hotmail.com>
2025-04-11 12:55:17 -0300

    Update test_get_element_fim.cpp

--- C/tests/arraylist/test_get_element_fim.cpp
+++ C/tests/arraylist/test_get_element_fim.cpp
@@ -26,7 +26,7 @@ auto test_get_fim(ArrayList& array_list, int n) {
 
 void test_dataset_get_fim(ArrayList& array_list, int num_execucoes) {
 
-    vector<int> values = {10000, 100000, 250000, 500000, 600000, 750000, 1000000, 1700000, 2500000, 3700000, 5000000, 6000000, 7500000, 90000000, 10000000};
+    vector<int> values = {10000, 100000, 250000, 500000, 600000, 750000, 1000000, 1700000, 2500000, 3700000, 5000000, 6000000, 7500000, 9000000, 10000000};
 
     string saida = "../out/arraylist/get_last.txt";
 
