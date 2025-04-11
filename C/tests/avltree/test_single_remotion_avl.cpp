ea3854646bc190941ba0e633ea2b33b799e9481e
PedroRamos1208 <pneto1999@hotmail.com>
2025-04-11 13:00:34 -0300

    Update test_single_remotion_avl.cpp

--- C/tests/avltree/test_single_remotion_avl.cpp
+++ C/tests/avltree/test_single_remotion_avl.cpp
@@ -20,7 +20,7 @@ auto test_single_removal(AVLTree& avl, int valor) {
 }
 
 void test_dataset_single_element_remotion(AVLTree& avl, int num_execucoes) {
-    vector<int> values = {10000, 100000, 250000, 500000, 600000, 750000, 1000000, 1700000, 2500000, 3700000, 5000000};
+    vector<int> values = {10000, 100000, 250000, 500000, 600000, 750000, 1000000, 1700000, 2500000, 3700000, 5000000, 6000000, 7500000, 9000000, 10000000};
 
     string saida = "../out/avltree/removal_single_element.txt";
 
