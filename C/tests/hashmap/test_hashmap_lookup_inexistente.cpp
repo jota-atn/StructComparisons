74011fe958f24c4cd89c768823082e0fc2a78285
PedroRamos1208 <pneto1999@hotmail.com>
2025-04-11 13:01:25 -0300

    Update test_hashmap_lookup_inexistente.cpp

--- C/tests/hashmap/test_hashmap_lookup_inexistente.cpp
+++ C/tests/hashmap/test_hashmap_lookup_inexistente.cpp
@@ -23,7 +23,7 @@ auto test_get_inexistente(HashMap& hashmap, int chave_inexistente, int num_execu
 }
 
 void test_lookup_inexistent(HashMap& hashmap, int num_execucoes) {
-    vector<int> values = {10000, 100000, 250000, 500000, 600000, 750000, 1000000, 1700000, 2500000, 3700000, 5000000};
+    vector<int> values = {10000, 100000, 250000, 500000, 600000, 750000, 1000000, 1700000, 2500000, 3700000, 5000000, 6000000, 7500000, 9000000, 10000000};
 
     string saida = "../out/hashmap/lookup_inexistent_element.txt";
     limpar_arquivo(saida);
